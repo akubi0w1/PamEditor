659eb39be058d2b28984a9b7e30f9e9104426158
yawn <hoge@example.com>
2019-10-05 23:30:35 +0900

    create prototype

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,16 @@ var sass = require("gulp-sass");
 
 gulp.task("sass", function(){
     return (
-        gulp.src("sass/**/*.scss")
+        gulp.src("src/sass/**/*.scss")
         .pipe(sass({outputStyle:"expanded"}))
         .pipe(gulp.dest("./src/css"))
     );
 });
 
 gulp.task("sass-watch", function(){
-    return gulp.watch("sass/**/*.scss", function(){
+    return gulp.watch("src/sass/**/*.scss", function(){
         return (
-            gulp.src("sass/**/*.scss")
+            gulp.src("src/sass/**/*.scss")
             .pipe(sass({outputStyle:"expanded"}).on("error", sass.logError))
             .pipe(gulp.dest("./src/css"))
         );
