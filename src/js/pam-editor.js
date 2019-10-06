b60cf1bab623e195318ad6501fb148e786d5f214
yawn <hoge@example.com>
2019-10-06 09:38:20 +0900

    create editor division

--- src/js/pam-editor.js
+++ src/js/pam-editor.js
@@ -1,7 +1,29 @@
-function PamEditor(el) {
-    console.log("call editor");
-    var box = document.createElement("div");
-    box.className = "box";
-    document.body.appendChild(box);
+function PamEditor(elem, options) {
+    var editor = document.createElement("div");
+    editor.id = elem;
+    editor.className = "PamEditor";
+    document.body.appendChild(editor);
+    console.log(options.height);
 
+    this.render();
+}
+
+PamEditor.prototype = {
+    render: function () {
+        this.createToolbar();
+    },
+
+    createToolbar: function () {
+        tools = [
+            "bold", "italic", "delete",
+        ];
+
+        // toolbarの生成？
+        var toolBlock = document.createElement("div");
+        toolBlock.className = "PamEditor-tools";
+
+        const setToolbar = tools.map(tool => {
+            document
+        });
+    },
 }
